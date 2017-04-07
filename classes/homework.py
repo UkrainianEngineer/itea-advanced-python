0651f458970a97d2a0bc6f62516db34136340560
Pavlo Ivanchyshyn <pavlo.ivanchyshyn@gmail.com>
2017-04-07 17:24:34 +0300

    Added homework.

---
+++ classes/homework.py
@@ -0,0 +1,19 @@
+#   Create base `Human` class with following fields: 
+#   `name`, `surname`, `age`, `gender` 
+# 
+#   Also add following methods: 
+#   `get_name`, `get_username`, `get_age`, `get_gender` 
+#   All of these methods should return values of appropriate fields. 
+# 
+#   Create class `Student` inherited from (based on) `Human` class. 
+#   `Student` class should have `marks` field as a list of their marks. 
+#   Also add `get_average_mark` method to this class. 
+#   It should calculate average mark for student. 
+# 
+#   Create class `Teacher` inherited from (based on) `Teacher` class. 
+#   `Teacher` class should have `classes` field 
+#   as a number of teacher's classes. 
+#   Also please add `get_number_of_classes` method to this class. 
+# 
+#   Create instance of each class, fill class data, 
+#   use appropriate methods of classes.
