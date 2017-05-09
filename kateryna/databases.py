d3fa716a800ee024ef0f99a086acf14f5db35f50
Катерина Уполовнікова <katedreamhealer@gmail.com>
2017-05-09 21:01:52 +0300

    Updates according to comments

--- kateryna/databases.py
+++ kateryna/databases.py
@@ -1,36 +1,82 @@
-class PostgreSQLConnectionRefusedError(Exception):
-    def __init__(self, message):
-        self.message = message
+class BaseExceptionHandler(Exception):
+    """Base class for framework exceptions."""
+    pass
+
+
+class PostgreSQLConnectionRefusedError(BaseExceptionHandler):
+    pass
+
+
+class ConnectionClosedError(BaseExceptionHandler):
+    pass
+
+
+class InvalidCommandError(BaseExceptionHandler):
+    pass
 
 
 class Connection(object):
+    """Provides dummy connection."""
     def __init__(self, db_name):
-        self.closed = False
+        """
+        Args:
+            db_name (str): Database name.
+        """
         self.db_name = db_name
 
     def __str__(self):
-        return "Connection to {}".format(self.db_name)
+        return "Connection to {}.".format(self.db_name)
 
 
 class BaseFramework(object):
+    """Base framework provides possibility to connect to database.
+    It can be used as a context manager:
+
+        with SQLiteApp(connection=Connection("my_db")) as app:
+            app.execute("select")
+
+    Attributes:
+        allowed_commands (list): Commands that can be executed.
+    """
     allowed_commands = ["SELECT", "INSERT", "UPDATE", "DELETE"]
 
     def __init__(self, connection, **kwargs):
+        """
+        Args:
+            connection (object): Connection instance.
+            **kwargs: Additional parameters that can be set as
+            attributes.
+        """
         self.connection = connection
         for attr, value in kwargs.items():
             setattr(self, attr, value)
 
     def connect(self):
-        self.connection.closed = False
         return self.connection
 
     def execute(self, command):
+        """
+        Args:
+            command (str): Database command.
+        Returns:
+            str: Valid database command.
+        """
+        if not self.connection:
+            raise ConnectionClosedError("Command cannot be executed, "
+                                        "the connection is closed.")
         if not command.upper() in self.allowed_commands:
-            raise Exception("Provide valid command")
+            raise InvalidCommandError("Provide valid command.")
         return command.upper()
 
     def close(self):
-        raise NotImplementedError
+        self.connection = None
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self, *args):
+        self.close()
 
 
 class SQLiteApp(BaseFramework):
@@ -41,63 +87,68 @@ class SQLiteApp(BaseFramework):
 
     def execute(self, command):
         command = super(SQLiteApp, self).execute(command)
-        return "SQLiteApp. {} command has been executed".format(command)
+        return "SQLiteApp. {} command has been executed.".format(command)
 
     def close(self):
-        self.connection.closed = True
-        return "{} closed".format(self.connection)
+        database_name = self.connection.db_name
+        super(SQLiteApp, self).close()
+        return "SQLiteApp. Connection to {} closed".format(database_name)
 
 
 class MySQLApp(BaseFramework):
 
     def connect(self):
         conn = super(MySQLApp, self).connect()
-        return 'SQLiteApp. {}'.format(conn)
+        return 'MySQLApp. {}'.format(conn)
 
     def execute(self, command):
         command = super(MySQLApp, self).execute(command)
-        return "MySQLApp. {} command has been executed".format(command)
+        return "MySQLApp. {} command has been executed.".format(
+            command
+        )
 
     def close(self):
-        self.connection.closed = True
-        return "MySQLApp. {} closed".format(self.connection)
+        database_name = self.connection.db_name
+        super(MySQLApp, self).close()
+        return "MySQLApp. Connection to {} closed.".format(
+            database_name
+        )
 
 
 class PostgreSQLApp(BaseFramework):
-    """"""
     def __init__(self, credentials, **kwargs):
         """
-        :param credentials: username and password
-        :type credentials: dict
+        Args:
+            credentials (dict): Username and password.
         """
         self.credentials = credentials
         super(PostgreSQLApp, self).__init__(**kwargs)
 
     def connect(self):
-        """ if any of credentials is missing, raise an error"""
-        if "username" not in self.credentials or "password" not in self.credentials:
-            raise PostgreSQLConnectionRefusedError("Provide username and passoword")
+        """ If any of credentials is missing, raise an error."""
+        if ("username" not in self.credentials
+                or "password" not in self.credentials):
+            raise PostgreSQLConnectionRefusedError("Provide username "
+                                                   "and password")
         return super(PostgreSQLApp, self).connect()
 
     def execute(self, command):
         command = super(PostgreSQLApp, self).execute(command)
-        return "PostgresSQLApp. {} command has been executed".format(command)
+        return "PostgresSQLApp. {} command has been executed.".format(command)
 
     def close(self):
-        return "PostgreSQLApp. Connection closed"
+        database_name = self.connection.db_name
+        super(PostgreSQLApp, self).close()
+        return "PostgreSQLApp. Connection to {} closed.".format(
+            database_name)
 
 
 sql = SQLiteApp(any_attr="some_value", connection=Connection(db_name="My Db"))
 print(sql.connect()) # connection to MyDb
 print(sql.execute("SELECT")) # executed
-# print(sql.execute("INNER JOIN")) #error, invalid command
-print(sql.connection.closed) #False
+# print(sql.execute("INNER JOIN")) # InvalidCommandError
 print(sql.close())
-print(sql.connection.closed) #True
-print(sql.connect())
-print(sql.connection.closed) #False
-print(sql.close())
-print(sql.connection.closed) #True
+# print(sql.execute("Select")) # ConnectionClosedError
 print('*' * 60)
 mysql = MySQLApp(connection=Connection(db_name="MySQL Db"))
 print(mysql.connect())
@@ -106,8 +157,9 @@ print(mysql.close())
 print("*" * 60)
 postgres = PostgreSQLApp(connection=Connection(db_name="My Db"), credentials={"username": '', "password": ''})
 # postgres = PostgreSQLApp(connection=Connection(db_name="My Db"), credentials={"username": ''}) #error
+# print(postgres.connect()) #PostgreSQLConnectionRefusedError
+# print(mysql.execute("Inner join")) #ConnectionClosedError
 print(postgres.connect())
-# # print(mysql.execute("Inner join"))
 print(postgres.execute("SELECT"))
 print(postgres.close())
 
