68c96fee6e1e94b7c7fb72fa22ba244491c95ee4
RomanPryima <pryima@stryi.com.ua>
2017-05-20 12:48:24 +0300

    rewritten docstrings

--- config_parser/config_reader.py
+++ config_parser/config_reader.py
@@ -1,14 +1,22 @@
 """
-Module for getting value of certain parameter in certain section in cfg. file
+
+Module for getting value of certain parameter in certain section in cfg. file.
+Uses ConfigParser module for Python.
+
 """
 import ConfigParser
 
 
 def get_setting(cfg_file, section,  parameter):
     """
-    cfg_file - name or path and name of cfg file for parsing
-    section - name of section of parameter
-    parameter - name of parameter of value has to be received
+    Args:
+        cfg_file - name or path and name of cfg file for parsing
+        section - name of section of parameter
+        parameter - name of parameter of value has to be received
+    Returns:
+        The return value. The value of requested parameter in requested
+        section.
+
     """
     config = ConfigParser.ConfigParser()
     config.read(cfg_file)
