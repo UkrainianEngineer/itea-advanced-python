c8961d4816d6285a49fc28758b4577096a9e612f
Sasha1152 <saxin@ukr.net>
2017-05-22 23:07:13 +0300

    change sorting condition of 'smart ordering'

--- travel_platform/smart_ordering.py
+++ travel_platform/smart_ordering.py
@@ -21,13 +21,7 @@ def smart_ordering(data_dict, filter_by=None, order_by='ASC', limit=100):
                              key=lambda note: (filter_by not in note,
                                                note.get(filter_by)),
                              reverse=True)
-        ind = 0
-        for note in sorted_data:
-            if note.get(filter_by) is not None:
-                ind = sorted_data.index(note)
-                break
-
-        del sorted_data[:ind]
+        sorted_data = filter(lambda note: note.get(filter_by), sorted_data)
 
     else:
         sorted_data = sorted(data_dict,
