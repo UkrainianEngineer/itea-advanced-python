cc92ec443a24627692a666d0aeb02033684f0728
Sasha1152 <saxin@ukr.net>
2017-05-25 16:42:23 +0300

    added func for filing database

--- sqlite/create_test_base.py
+++ sqlite/create_test_base.py
@@ -3,13 +3,14 @@ import sqlite3
 #Подключение к базе
 conn = sqlite3.connect('test_base.sqlite')
 #Создание курсора
-c = conn.cursor()
+cur = conn.cursor()
 #Создание таблицы
-c.execute('''CREATE TABLE users (id int auto_increment primary key,name varchar, password varchar)''')
+cur.execute('''CREATE TABLE users (id int auto_increment primary key,
+               name varchar, password varchar)''')
 #Наполнение таблицы
-c.execute("INSERT INTO users (name,password) VALUES ('admin','123')")
+cur.execute("INSERT INTO users (name, password) VALUES ('admin', '123')")
 #Подтверждение отправки данных в базу
 conn.commit()
 #Завершение соединения
-c.close()
+cur.close()
 conn.close()
---
+++ sqlite/create_test_data_for_base.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+
+conn = sqlite3.connect('test_base.sqlite')
+cur = conn.cursor()
+
+# Функция занесения пользователя в базу
+def add_user(username, userpass):
+    cur.execute("INSERT INTO users (name, password) VALUES ('%s', '%s')"%(username, userpass))
+    conn.commit()
+
+#Вводим данные
+name = raw_input("Enter your name: ")
+password = raw_input("Enter password: ")
+
+#Делаем запрос в базу
+print("Users list:")
+add_user(name, password)
+cur.execute('SELECT * FROM users')
+row = cur.fetchone()
+
+#выводим список пользователей в цикле
+while row is not None:
+   print("id:"+str(row[0])+" mame: "+row[1]+" | password: "+row[2])
+   row = cur.fetchone()
+
+# закрываем соединение с базой
+cur.close()
+conn.close()
