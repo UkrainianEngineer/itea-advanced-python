b0ce5a4aa49a581b47632894c5df591b3285a429
Sasha1152 <saxin@ukr.net>
2017-05-25 09:15:42 +0300

    fixed docstring

--- travel_platform/smart_ordering.py
+++ travel_platform/smart_ordering.py
@@ -1,16 +1,16 @@
 
 def smart_ordering(data_dict, filter_by=None, order_by='ASC', limit=100):
-    '''
+    """
     Sorting data by different conditions.
     Args:
-        data (list): List of dictionaries with data to sort.
+        data_dict (list): List of dictionaries with data to sort.
         filter_by (str): Filtering attribute.
         order_by (str:ASC|DESC): Filtering order.
         limit (int): Limitation of data to return.
 
     Returns:
         list: Sorted list of dictionaries with applied filters.
-    '''
+    """
     order_by = order_by.upper()
 
     if filter_by is None:
@@ -27,6 +27,5 @@ def smart_ordering(data_dict, filter_by=None, order_by='ASC', limit=100):
         sorted_data = sorted(data_dict,
                              key=lambda note: (filter_by not in note,
                                                note.get(filter_by)))
-
 
     return sorted_data[:limit]
