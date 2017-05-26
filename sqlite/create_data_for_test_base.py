e600ff66a6ac3d2ce9d68d8c2d5001e9934639f3
Sasha1152 <saxin@ukr.net>
2017-05-26 17:52:03 +0300

    work with databases

--- sqlite/create_data_for_test_base.py
+++ sqlite/create_data_for_test_base.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 
-conn = sqlite3.connect('test_base.sqlite')
+conn = sqlite3.connect('test_base2.sqlite')
 cur = conn.cursor()
 
 # Функция занесения пользователя в базу
 def add_user(username, userpass):
-    cur.execute("INSERT INTO users (name, password) VALUES ('%s', '%s')"%(username, userpass))
+    cur.execute("INSERT INTO users (name, password) VALUES ('%s', '%s')"
+                %(username, userpass))
     conn.commit()
 
-#Вводим данные
+# Вводим данные
 name = raw_input("Enter your name: ")
 password = raw_input("Enter password: ")
 
-#Делаем запрос в базу
+# Делаем запрос в базу
 print("Users list:")
 add_user(name, password)
 cur.execute('SELECT * FROM users')
 row = cur.fetchone()
 
-#выводим список пользователей в цикле
+# выводим список пользователей в цикле
 while row is not None:
-   print("id:"+str(row[0])+" mame: "+row[1]+" | password: "+row[2])
+   print("id:"+str(row[0])+"| mame: "+row[1]+" | password: "+row[2])
    row = cur.fetchone()
 
 # закрываем соединение с базой
--- sqlite/create_test_base.py
+++ sqlite/create_test_base.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 import sqlite3
-#Подключение к базе
-conn = sqlite3.connect('test_base.sqlite')
-#Создание курсора
+# Подключение к базе
+conn = sqlite3.connect('test_base2.sqlite')
+# Создание курсора
 cur = conn.cursor()
-#Создание таблицы
-cur.execute('''CREATE TABLE users (id int auto_increment primary key,
-               name varchar, password varchar)''')
-#Наполнение таблицы
-cur.execute("INSERT INTO users (name, password) VALUES ('admin', '123')")
-#Подтверждение отправки данных в базу
+# Создание таблицы
+cur.execute('CREATE TABLE users (id integer not null primary key autoincrement,'
+            'name varchar(20), password varchar(12))')
+
+# Наполнение таблицы
+cur.execute('INSERT INTO users (name, password) VALUES ("admin", "123")')
+
+# Подтверждение отправки данных в базу
 conn.commit()
-#Завершение соединения
+# Завершение соединения
 cur.close()
 conn.close()
