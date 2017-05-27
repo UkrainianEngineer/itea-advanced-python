95f0316efcae5545df0ffae5724b652bf4994083
RomanPryima <pryima@stryi.com.ua>
2017-05-27 11:35:21 +0300

    migrated db, made admin interface, created testing simple model

--- travel_platform/travel_app/models.py
+++ travel_platform/travel_app/models.py
@@ -2,3 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
+
+
+class Query(models.Model):
+    """docstring for ClassName"""
+    available_money = models.CharField(max_length=6)
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.query_text
