d256d5e200af81928d910ecd6644b68ad13f07f5
sasha <saxin@ukr.net>
2017-05-29 23:41:55 +0300

    deamon process

--- homework/rain_process.sh
+++ homework/rain_process.sh
@@ -6,20 +6,20 @@
 
 daemonName="RAIN"
 
-pidDir="."
-pidFile="$pidDir/$daemonName.pid"
-pidFile="$daemonName.pid"
+pidDir="rain_pid"
+pidFile="$pidDir/$RAIN.pid"
+pidFile="$RAIN.pid"
 
-logDir="."
+logDir="rain_log"
 # To use a dated log file.
 # logFile="$logDir/$daemonName-"`date +"%Y-%m-%d"`".log"
 # To use a regular log file.
-logFile="$logDir/$daemonName.log"
+logFile="$logDir/$RAIN.log"
 
 # Log maxsize in KB
 logMaxSize=1024   # 1mb
 
-runInterval=60 # In seconds
+runInterval=5 # In seconds
 
 doCommands() {
   # This is where you put all the commands for the daemon.
@@ -56,12 +56,12 @@ startDaemon() {
   # Start the daemon.
   setupDaemon # Make sure the directories are there.
   if [[ `checkDaemon` = 1 ]]; then
-    echo " * \033[31;5;148mError\033[39m: $daemonName is already running."
+    echo " * \033[31;5;148mError\033[39m: $RAIN is already running."
     exit 1
   fi
-  echo " * Starting $daemonName with PID: $myPid."
+  echo " * Starting $RAIN with PID: $myPid."
   echo "$myPid" > "$pidFile"
-  log '*** '`date +"%Y-%m-%d"`": Starting up $daemonName."
+  log '*** '`date +"%Y-%m-%d"`": Starting up $RAIN."
 
   # Start the loop.
   loop
@@ -70,11 +70,11 @@ startDaemon() {
 stopDaemon() {
   # Stop the daemon.
   if [[ `checkDaemon` -eq 0 ]]; then
-    echo " * \033[31;5;148mError\033[39m: $daemonName is not running."
+    echo " * \033[31;5;148mError\033[39m: $RAIN is not running."
     exit 1
   fi
-  echo " * Stopping $daemonName"
-  log '*** '`date +"%Y-%m-%d"`": $daemonName stopped."
+  echo " * Stopping $RAIN"
+  log '*** '`date +"%Y-%m-%d"`": $RAIN stopped."
 
   if [[ ! -z `cat $pidFile` ]]; then
     kill -9 `cat "$pidFile"` &> /dev/null
@@ -84,9 +84,9 @@ stopDaemon() {
 statusDaemon() {
   # Query and return whether the daemon is running.
   if [[ `checkDaemon` -eq 1 ]]; then
-    echo " * $daemonName is running."
+    echo " * $RAIN is running."
   else
-    echo " * $daemonName isn't running."
+    echo " * $RAIN isn't running."
   fi
   exit 0
 }
@@ -95,7 +95,7 @@ restartDaemon() {
   # Restart the daemon.
   if [[ `checkDaemon` = 0 ]]; then
     # Can't restart it if it isn't running.
-    echo "$daemonName isn't running."
+    echo "$RAIN isn't running."
     exit 1
   fi
   stopDaemon
@@ -119,9 +119,9 @@ checkDaemon() {
         return 0
       fi
     fi
-  elif [[ `ps aux | grep "$daemonName" | grep -v grep | grep -v "$myPid" | grep -v "0:00.00"` > /dev/null ]]; then
+  elif [[ `ps aux | grep "$RAIN" | grep -v grep | grep -v "$myPid" | grep -v "0:00.00"` > /dev/null ]]; then
     # Daemon is running but without the correct PID. Restart it.
-    log '*** '`date +"%Y-%m-%d"`": $daemonName running with invalid PID; restarting."
+    log '*** '`date +"%Y-%m-%d"`": $RAIN running with invalid PID; restarting."
     restartDaemon
     return 1
   else
