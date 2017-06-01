5de2554c1687dc5e4f5ef41e301fea0cfd3dba2d
Катерина Уполовнікова <katedreamhealer@gmail.com>
2017-06-01 10:29:53 +0300

    Get real data from izitravel

---
+++ travel_platform/izitravel_data.py
@@ -0,0 +1,24 @@
+from config_parser.config_reader import get_setting
+from conf import CONF_PATH
+from izi_travel_client import IziTravelApiClient
+
+
+client = IziTravelApiClient(api_key=get_setting(CONF_PATH,
+                                                "api.izi.travel",
+                                                "API_KEY"))
+
+
+def find_museums(city):
+    return client.get_city_museums(city)
+
+
+def find_museum_detail(museum_uuid):
+    return client.get_museum_detail_with_audio(museum_uuid)
+
+
+def find_city_tours(city):
+    return client.get_city_tours(city)
+
+
+def find_tour_attractions(tour_uuid):
+    return client.get_tourist_attractions(tour_uuid)
