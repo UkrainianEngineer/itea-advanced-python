ac81a6645544506a5828ebd0810cbbbeae876208
Sasha1152 <saxin@ukr.net>
2017-06-11 00:12:17 +0300

    work with pathes

--- package/__init__.py
+++
@@ -1,2 +0,0 @@
-from module_2 import func_2
-from module_3 import func_3
--- package/package_1/module_1-2.py
+++ package/package_1/module_1-2.py
@@ -1,5 +1,13 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import module_2
 import module_3
 
+# from .. import module_2
+# from .. import module_3
+
 module_2.func_2()
 module_3.func_3()
+
+sys.path.pop()
