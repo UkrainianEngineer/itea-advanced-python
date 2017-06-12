c1a813084d472556c5a2d2808a04f59051e8ac5d
Sasha1152 <saxin@ukr.net>
2017-06-12 19:12:42 +0300

    change code

--- travel_platform/skyscanner_data/skyscanner_data.py
+++ travel_platform/skyscanner_data/skyscanner_data.py
@@ -5,28 +5,64 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from skyscanner.skyscanner import FlightsCache
 from conf import config
 
-
-class SkyscannerApiClient(object):
-
-    def __init__(self, api_key, market, currency, locale, origin_place,
-                 destination_place, outbound_date, inbound_date):
-
-        self.api_key = config.get('api_skyscanner', 'api_key')
-        self.market = config.get('api_skyscanner', 'market')
-        self.currency = config.get('api_skyscanner', 'currency')
-        self.locale = config.get('api_skyscanner', 'locale')
-        self.origin_place = config.get('api_skyscanner', 'origin_place')
-        self.destination_place = config.get('api_skyscanner', 'destination_place')
-        self.outbound_date = config.get('api_skyscanner', 'outbound_date')
-        self.inbound_date = config.get('api_skyscanner', 'inbound_date')
+api_key = config.get('api_skyscanner', 'api_key')
+market = config.get('api_skyscanner', 'market')
+currency = config.get('api_skyscanner', 'currency')
+locale = config.get('api_skyscanner', 'locale')
+origin_place = config.get('api_skyscanner', 'origin_place')
+destination_place = config.get('api_skyscanner', 'destination_place')
+outbound_date = config.get('api_skyscanner', 'outbound_date')
+inbound_date = config.get('api_skyscanner', 'inbound_date')
+flights_cache_service = FlightsCache(api_key)
 
 
-    def flights_cache_service(self):
+def cheapest_quotes():
+    result = flights_cache_service.get_cheapest_quotes(
+        market='UK',
+        currency='GBP',
+        locale='en-GB',
+        originplace='SIN-sky',
+        destinationplace='KUL-sky',
+        outbounddate='2017-07-15',
+        inbounddate='2017-07-16').parsed
+    return result
 
-        flights_cache_service = FlightsCache(self.api_key)
-        return flights_cache_service.get_cheapest_quotes().parsed
 
-response = SkyscannerApiClient()
-# sys.path.pop()
+def cheapest_price_by_route():
+    result = flights_cache_service.get_cheapest_price_by_route(
+        market='UK',
+        currency='GBP',
+        locale='en-GB',
+        originplace='SIN-sky',
+        destinationplace='KUL-sky',
+        outbounddate='2017-07-15',
+        inbounddate='2017-07-16').parsed
 
-print response
+
+def cheapest_price_by_date():
+    result = flights_cache_service.get_cheapest_price_by_date(
+        market='UK',
+        currency='GBP',
+        locale='en-GB',
+        originplace='SIN-sky',
+        destinationplace='KUL-sky',
+        outbounddate='2017-07-15',
+        inbounddate='2017-07-16').parsed
+
+
+def grid_prices_by_date():
+    result = flights_cache_service.get_grid_prices_by_date(
+        market='UK',
+        currency='GBP',
+        locale='en-GB',
+        originplace='SIN-sky',
+        destinationplace='KUL-sky',
+        outbounddate='2017-07-15',
+        inbounddate='2017-07-16').parsed
+
+# print cheapest_quotes()
+# print cheapest_price_by_route()
+# print cheapest_price_by_date()
+# print grid_prices_by_date()
+
+sys.path.pop()
