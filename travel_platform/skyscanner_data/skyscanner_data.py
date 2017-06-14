47fe10187507e9ac568c86a0f4a4328733bbbcbf
Sasha1152 <saxin@ukr.net>
2017-06-14 23:38:33 +0300

    change code

--- travel_platform/skyscanner_data/skyscanner_data.py
+++ travel_platform/skyscanner_data/skyscanner_data.py
@@ -6,50 +6,43 @@ from skyscanner.skyscanner import FlightsCache
 from conf import config
 
 api_key = config.get('api_skyscanner', 'api_key')
-market = config.get('api_skyscanner', 'market')
-currency = config.get('api_skyscanner', 'currency')
-locale = config.get('api_skyscanner', 'locale')
-origin_place = config.get('api_skyscanner', 'origin_place')
-destination_place = config.get('api_skyscanner', 'destination_place')
-outbound_date = config.get('api_skyscanner', 'outbound_date')
-inbound_date = config.get('api_skyscanner', 'inbound_date')
+
 flights_cache_service = FlightsCache(api_key)
 
-#FIXME: why my *args not work?
+#FIXME: why my *kwargs not work? TypeError: get_cheapest_quotes() takes exactly 1 argument (2 given)
 
-def cheapest_quotes(*args):
-    result = flights_cache_service.get_cheapest_quotes(args).parsed
-    return result
+def cheapest_quotes(**kwargs):
+    return flights_cache_service.get_cheapest_quotes(kwargs).parsed
 
+# print flights_cache_service.get_cheapest_quotes(market = 'UK', currency = 'GBP', locale = 'en-GB',
+#                       originplace = 'SIN-sky', destinationplace = 'KUL-sky',
+#                       outbounddate = '2017-07-15', inbounddate = '2017-07-18').parsed
 
-def cheapest_price_by_route(*args):
-    result = flights_cache_service.get_cheapest_price_by_route(args).parsed
-    return result
-
-
-def cheapest_price_by_date(*args):
-    result = flights_cache_service.get_cheapest_price_by_date(args).parsed
-    return result
+def cheapest_price_by_route(**kwargs):
+    return flights_cache_service.get_cheapest_price_by_route(kwargs).parsed
 
 
-def grid_prices_by_date(*args):
-    result = flights_cache_service.get_grid_prices_by_date(args).parsed
-    return result
+def cheapest_price_by_date(**kwargs):
+    return flights_cache_service.get_cheapest_price_by_date(kwargs).parsed
 
-print cheapest_quotes(market, currency, locale, origin_place,
-                      destination_place, outbound_date,
-                      inbound_date)
 
-print cheapest_price_by_route(market, currency, locale, origin_place,
-                      destination_place, outbound_date,
-                      inbound_date)
+def grid_prices_by_date(**kwargs):
+    return flights_cache_service.get_grid_prices_by_date(kwargs).parsed
 
-print cheapest_price_by_date(market, currency, locale, origin_place,
-                      destination_place, outbound_date,
-                      inbound_date)
+print cheapest_quotes(market = 'UK', currency = 'GBP', locale = 'en-GB',
+                      originplace = 'SIN-sky', destinationplace = 'KUL-sky',
+                      outbounddate = '2017-07-15', inbounddate = '2017-07-18')
 
-print grid_prices_by_date(market, currency, locale, origin_place,
-                      destination_place, outbound_date,
-                      inbound_date)
+print cheapest_price_by_route(market = 'UK', currency = 'GBP', locale = 'en-GB',
+                      originplace = 'SIN-sky', destinationplace = 'KUL-sky',
+                      outbounddate = '2017-07-15', inbounddate = '2017-07-18')
+
+print cheapest_price_by_date(market = 'UK', currency = 'GBP', locale = 'en-GB',
+                      originplace = 'SIN-sky', destinationplace = 'KUL-sky',
+                      outbounddate = '2017-07-15', inbounddate = '2017-07-18')
+
+print grid_prices_by_date(market = 'UK', currency = 'GBP', locale = 'en-GB',
+                      originplace = 'SIN-sky', destinationplace = 'KUL-sky',
+                      outbounddate = '2017-07-15', inbounddate = '2017-07-18')
 
 sys.path.pop()
