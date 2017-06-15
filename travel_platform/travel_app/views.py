eef4afbb61898c435fdc3671c11653334d101a5a
RomanPryima <pryima@stryi.com.ua>
2017-06-15 16:04:25 +0300

    first commit

--- travel_platform/travel_app/views.py
+++ travel_platform/travel_app/views.py
@@ -9,6 +9,8 @@ from django.shortcuts import render
 
 from .models import Query
 
+import requests
+
 
 def index(request):
     return render(request, 'travel_app/main.html')
@@ -16,15 +18,26 @@ def index(request):
 
 def coord(request):
     """
-    View added for checking of interaction of map from landing page with 
-    server.
-    :param request: 
-    :return: 
+    This view receives coordinates of location from the GET request, sends it 
+    to the Open Maps API. Than returns name of the location received from API 
+    to the frontend.
+    
+    :param request: data received from frontend, containing longitude and
+      latitude
+    :return: name of location gotten from open maps API
     """
 
-    """TODO RomanPryima: make function sending coordinates and returning 
-    to the frontend name of location received from map API"""
+    # TO DO RomanPryima: move url and api key into cfg file and make view
+    # getting them with cfg parser
 
-    response = request.GET.get('lon') + " " + request.GET.get('lat')
+    api_url = 'http://open.mapquestapi.com/geocoding/v1/reverse'
+    api_key = 'lrGqjWqt82ZGMvQPA0SPXqctacNvptot'
+    coordinate_lon = request.GET.get('lon')
+    coordinate_lat = request.GET.get('lat')
 
+    location_request = requests.get(str(api_url + '?key=' + api_key +
+                                        '&location=' + coordinate_lat + ','
+                                        + coordinate_lon))
+    location_data = location_request.json()
+    response = str(location_data['results'][0]['locations'][0]['adminArea5'])
     return JsonResponse(response, safe=False)
