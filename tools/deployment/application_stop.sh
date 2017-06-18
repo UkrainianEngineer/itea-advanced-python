4b16d345fd45062f0ccf123f21c224e1a563f11b
Pavlo Ivanchyshyn <pavlo.ivanchyshyn@gmail.com>
2017-06-18 16:25:05 +0300

    Minor change.

--- tools/deployment/application_stop.sh
+++ tools/deployment/application_stop.sh
@@ -1,4 +1,5 @@
 # TODO add stopping of webserver, cleaning directories after
 # `python setup.py install` installation process.
+echo "Making directory..."
 mkdir -p /usr/local/etc/travel_platform
 echo "Created folder for configs: /usr/local/etc/travel_platform"
