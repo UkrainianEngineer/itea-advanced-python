2fb094ea78119e0a3c7a1f2801f8ea396648dea8
Pavlo Ivanchyshyn <pavlo.ivanchyshyn@gmail.com>
2017-06-18 14:11:10 +0300

    Added some prerequisites for deployment.

--- tools/deployment/application_after_install.sh
+++ tools/deployment/application_after_install.sh
@@ -1,1 +1,38 @@
 # TODO execute `python setup.py install` command.
+# Update and upgrade package repositories.
+sudo yum update
+sudo yum upgrade
+
+# Install Python and some basic packages.
+sudo yum install python-pip
+sudo yum install python-dev
+sudo yum install build-essential
+
+# Install virtualenvwrapper
+sudo pip install virtualenvwrapper
+
+if [[ ! -s "$HOME/.bash_profile" && -s "$HOME/.profile" ]] ; then
+  profile_file="$HOME/.profile"
+else
+  profile_file="$HOME/.bash_profile"
+fi
+
+if ! grep -q 'virtualenvwrapper.sh' "${profile_file}" ; then
+  echo "export WORKON_HOME=$HOME/.virtualenvs" >> "${profile_file}"
+  echo "source /usr/local/bin/virtualenvwrapper.sh" >> "${profile_file}"
+fi
+
+source ${profile_file}
+
+echo "Current folder" + pwd
+
+# Prepare virtual environment.
+mkvirtualenv travel_platform
+workon travel_platform
+
+# Install packages.
+pip install -r requirements.txt
+
+# Install custom packages.
+python setup.py install
+
