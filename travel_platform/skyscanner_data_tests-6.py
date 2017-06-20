3a50e8c6625c6f13068fe6a96ae18eb846bb5ce5
sasha <saxin@ukr.net>
2017-06-20 23:33:45 +0300

    added trial files

---
+++ travel_platform/skyscanner_data_tests-6.py
@@ -0,0 +1,36 @@
+import unittest
+
+from skyscanner_data import SkyscannerData
+
+# FIXME: Sasha: Fix all these tests, they fails because of invalid parameters.
+
+
+class SkyscannerDataTest(unittest.TestCase):
+    def setUp(self):
+        self.data = {"market": "UK", "currency": "GBP", "locale": "en-GB",
+                     "originplace": "SIN-sky", "destinationplace": "KUL-sky",
+                     "outbounddate": "2017-07-15", "inbounddate": "2017-07-16"}
+
+    def test_cheapest_quotes(self):
+        actual_result = SkyscannerData.cheapest_quotes(**self.data)
+        expected_result = {}
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_cheapest_price_by_route(self):
+        actual_result = SkyscannerData.cheapest_price_by_route(**self.data)
+        expected_result = {}
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_cheapest_price_by_date(self):
+        actual_result = SkyscannerData.cheapest_price_by_date(**self.data)
+        expected_result = {}
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_grid_prices_by_date(self):
+        actual_result = SkyscannerData.grid_prices_by_date(**self.data)
+        expected_result = {}
+
+        self.assertEqual(actual_result, expected_result)
