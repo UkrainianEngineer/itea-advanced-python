c6337f746ca8c39a76fd8a8daa8981f43d6a4b0f
Pavlo Ivanchyshyn <pavlo.ivanchyshyn@gmail.com>
2017-06-21 23:54:04 +0300

    Added some stup scripts for MySQL in container. Need to be tested under linux.

---
+++ databases/0.prepare_env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ "$(uname)" == "Darwin" ]; then
+  brew install mysql
+  export PATH=$PATH:/usr/local/mysql/bin
+  pip install MySQL-Python
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  sudo apt-get install libmysqlclient-dev
+  sudo apt-get -y install docker.io
+  export PATH=$PATH:/usr/local/mysql/bin
+  pip install MySQL-Python
+fi
---
+++ databases/setup_sql_container.sh
@@ -0,0 +1,14 @@
+# Download MySQL image from hub.docker.com.
+#docker pull mysql/mysql-server
+
+# Stop mysql-container
+[ "$(docker ps | grep mysql-container)" ] && docker stop mysql-container
+
+# Remove mysql-container
+[ "$(docker ps | grep mysql-container)" ] && docker rm mysql-container
+
+
+[ "$(docker ps -a | grep mysql-container)" ] && docker ps -a| grep mysql-container| awk '{print $1}'| xargs docker rm
+
+# Run MySQL container in user's system.
+docker run --name mysql-container -p 3306:3306 -e MYSQL_ROOT_PASSWORD=secret -e MYSQL_ROOT_HOST="%" -d mysql/mysql-server:latest
