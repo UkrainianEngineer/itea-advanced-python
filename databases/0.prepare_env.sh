69cb142f413b4dad7576ee05842456b5c66a7e0d
vagrant <vagrant@vagrant.vm>
2017-06-21 22:19:11 +0000

    Added some comments and one more installation

--- databases/0.prepare_env.sh
+++ databases/0.prepare_env.sh
@@ -6,6 +6,7 @@ if [ "$(uname)" == "Darwin" ]; then
   pip install MySQL-Python
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   sudo apt-get install libmysqlclient-dev
+  sudo apt-get install mysql-client
   sudo apt-get -y install docker.io
   export PATH=$PATH:/usr/local/mysql/bin
   pip install MySQL-Python
