157a318696696b1c6914ec74ee2ab6a07acbb1b0
Sasha1152 <saxin@ukr.net>
2017-06-24 00:23:01 +0300

    added some tests for five fingers

--- homework/five_fingers_def.py
+++ homework/five_fingers_def.py
@@ -3,10 +3,10 @@ def finger_name(number, fingers):
     remainder = number % ((len(fingers) - 1) * 2)
 
     if remainder <= (len(fingers) - 1):
-        print 'first case: ', fingers[remainder]
+        return fingers[remainder]
 
     if remainder > (len(fingers) - 1):
-        print 'second case: ', fingers[-(remainder - 3)]
+        return fingers[-(remainder - 3)]
 
-# finger_name(1, ['pinky', 'ring', 'middle', 'index', 'thumb'])
-# finger_name(8, ['pinky', 'ring', 'middle', 'index'])
+# print finger_name(1, ['pinky', 'ring', 'middle', 'index', 'thumb'])
+# print finger_name(12, ['pinky', 'ring', 'middle', 'index'])
--- homework/five_fingers_test.py
+++ homework/five_fingers_test.py
@@ -18,5 +18,53 @@ class FiveFingersTest(unittest.TestCase):
         result = finger_name(9, ['pinky', 'ring', 'middle', 'index', 'thumb'])
         self.assertEqual(result, 'pinky')
 
+    def test_five_fingers_n1992(self):
+        result = finger_name(1992, ['pinky', 'ring', 'middle', 'index', 'thumb'])
+        self.assertEqual(result, 'ring')
+
+    def test_four_fingers_n4(self):
+        result = finger_name(4, ['pinky', 'ring', 'middle', 'index'])
+        self.assertEqual(result, 'index')
+
+    def test_four_fingers_n12(self):
+        result = finger_name(12, ['pinky', 'ring', 'middle', 'index'])
+        self.assertEqual(result, 'ring')
+
+    def test_four_fingers_n8(self):
+        result = finger_name(8, ['pinky', 'ring', 'middle', 'index'])
+        self.assertEqual(result, 'ring')
+
+    def test_four_fingers_n24(self):
+        result = finger_name(24, ['pinky', 'ring', 'middle', 'index'])
+        self.assertEqual(result, 'ring')
+
+    def test_four_fingers_n6(self):
+        result = finger_name(6, ['pinky', 'ring', 'middle', 'index'])
+        self.assertEqual(result, 'ring')
+
+    def test_four_fingers_n7(self):
+        result = finger_name(7, ['pinky', 'ring', 'middle', 'index'])
+        self.assertEqual(result, 'pinky')
+
+    def test_three_fingers_n3(self):
+        result = finger_name(3, ['pinky', 'ring', 'middle'])
+        self.assertEqual(result, 'middle')
+
+    def test_three_fingers_n5(self):
+        result = finger_name(5, ['pinky', 'ring', 'middle'])
+        self.assertEqual(result, 'pinky')
+
+    def test_three_fingers_n10(self):
+        result = finger_name(10, ['pinky', 'ring', 'middle'])
+        self.assertEqual(result, 'ring')
+
+    def test_seven_fingers_n7(self):
+        result = finger_name(7, ['minipinky', 'pinky', 'ring', 'middle', 'index', 'thumb', 'megathumb'])
+        self.assertEqual(result, 'megathumb')
+
+    def test_seven_fingers_n30(self):
+        result = finger_name(30, ['minipinky', 'pinky', 'ring', 'middle', 'index', 'thumb', 'megathumb'])
+        self.assertEqual(result, 'thumb')
+
 if __name__ == '__main__':
     unittest.main()
