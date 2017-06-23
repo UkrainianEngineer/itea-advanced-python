15d8a6dcadd0c64037f19d8e4c06034d73f0987e
Sasha1152 <saxin@ukr.net>
2017-06-23 23:54:39 +0300

    work

--- homework/five_fingers_test.py
+++ homework/five_fingers_test.py
@@ -6,10 +6,17 @@ class FiveFingersTest(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_five_fingers_n1_pinky(self):
+    def test_five_fingers_n1(self):
         result = finger_name(1, ['pinky', 'ring', 'middle', 'index', 'thumb'])
         self.assertEqual(result, 'pinky')
 
-    def test_five_fingers_n1_not_pinky(self):
-        result = finger_name(1, ['pinky', 'ring', 'middle', 'index', 'thumb'])
-        self.assertNotEqual(result, 'pinky')
+    def test_five_fingers_n2(self):
+        result = finger_name(2, ['pinky', 'ring', 'middle', 'index', 'thumb'])
+        self.assertEqual(result, 'ring')
+
+    def test_five_fingers_n9(self):
+        result = finger_name(9, ['pinky', 'ring', 'middle', 'index', 'thumb'])
+        self.assertEqual(result, 'pinky')
+
+if __name__ == '__main__':
+    unittest.main()
