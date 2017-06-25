fe87a4b250f1ad3e39de351ac51147865cb175ea
Pavlo Ivanchyshyn <pavlo.ivanchyshyn@gmail.com>
2017-06-25 23:34:03 +0300

    Fixed MySQL scripts for OS X system.

--- databases/0.prepare_env.sh
+++ databases/0.prepare_env.sh
@@ -4,6 +4,11 @@ if [ "$(uname)" == "Darwin" ]; then
   brew install mysql
   export PATH=$PATH:/usr/local/mysql/bin
   pip install MySQL-Python
+  brew install docker
+  brew install boot2docker
+  boot2docker init
+  boot2docker up
+  eval "$(boot2docker shellinit)"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   sudo apt-get install libmysqlclient-dev
   sudo apt-get install mysql-client
--- databases/connect_to_sql.sh
+++ databases/connect_to_sql.sh
@@ -1,1 +1,1 @@
-mysql -h127.0.0.1 -uroot -psecret -P 3306
+docker exec -it mysql-container mysql -h127.0.0.1 -uroot -psecret -P 3306
