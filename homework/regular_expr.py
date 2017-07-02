376dcde00d3cee7e42bbfc9e9cff185b01b1c80b
Sasha1152 <saxin@ukr.net>
2017-07-02 19:26:23 +0300

    work with RegExp

--- homework/regular_expr.py
+++ homework/regular_expr.py
@@ -15,14 +15,28 @@ We have heard that Mr. Harris\
 Intends to send you off as far as\
 The great world's show at Paris.\
 Of the youth beware of these,\
-For some of them might rudely squeeze\
-And bite your cheek, then songs or glees\
+For some of them might rudely squeeze \
+And bite your cheek, then songs or glees \
 We could not sing, oh! queen of cheese.\
 We'rt thou suspended from balloon,\
 You'd cast a shade even at noon,\
 Folks would think it was the moon\
-About to fall and crush them soon."\
+About to fall and crush them soon."
 
 import re
-
-print re.findall('c[a-z]*', vers, re.IGNORECASE)
+# all words, which beginning of letter 'c'
+print re.findall(r'\bc[a-z]*', vers, re.IGNORECASE)
+# the same
+print re.findall(r'\bc\w*', vers, re.I)
+# all words, which beginning of letter 'c' and have four letters
+print re.findall(r'\bc[a-z]{3}\b', vers, re.IGNORECASE)
+# the same
+print re.findall(r'\bc\w{3}\b', vers, re.I)
+# all words, which ending of letter 'r'
+print re.findall(r'\w+r\b', vers, re.IGNORECASE)
+# all words, which ending of letter 'l'
+print re.findall(r'[\'\w]+l\b', vers, re.IGNORECASE)
+# the same
+print re.findall(r"['\w]+l\b", vers, re.IGNORECASE)
+# the words which have three consonant letters successively
+print re.findall('\w*[aeiou]{3}\w*', vers, re.IGNORECASE)
