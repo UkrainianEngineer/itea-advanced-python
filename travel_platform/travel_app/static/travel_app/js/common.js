35bdc25c5efc6d4c03e05de0859e639d531394fc
Катерина Уполовнікова <katedreamhealer@gmail.com>
2017-08-07 20:23:58 +0300

    edited common.js

--- travel_platform/travel_app/static/travel_app/js/common.js
+++ travel_platform/travel_app/static/travel_app/js/common.js
@@ -1,8 +1,17 @@
 $(function () {
-    $("#search-city-form").submit(function (e) {
-        e.preventDefault();
-        var city = $("#search-city").val();
-        var urlPart = $("#search-city-btn").data('urlPart');
-        document.location.href = urlPart + city;
-    })
+    function redirectToCityUrl(elementId) {
+        $(elementId).submit(function (e) {
+            e.preventDefault();
+            var city = $("#search-city").val();
+            var urlPart = $("#search-city-btn").data('urlPart');
+            document.location.href = urlPart + city;
+
+        })
+
+    }
+
+    redirectToCityUrl("#search-attractions-form");
+    redirectToCityUrl("#search-museums-form");
+    redirectToCityUrl("#search-foursquare-form");
+
 });
--- travel_platform/travel_app/static/travel_app/js/museums.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $("#museums-city").submit(function (e) {
-        e.preventDefault();
-        var city = $("#search-city").val();
-        var urlPart = $("#search-city-btn").data('urlPart');
-        document.location.href = urlPart + city;
-    })
-});
